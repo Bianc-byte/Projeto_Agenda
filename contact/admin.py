2be23e8679ef2b38304bdf310db441a5d225578c
Bianca Bueno <biancacunha326@gmail.com>
2025-08-07 23:08:23 -0300

    Adicionando contacts

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date')
+    ordering = ('id',)
+    search_fields = ('id','first_name', 'last_name', 'phone', 'email')
+    list_per_page = 10
+    list_max_show_all = 200
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('id',)
