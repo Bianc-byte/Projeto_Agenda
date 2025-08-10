5e4c65cef5e2f76041b0d66e594bfc32b6b5eedc
Bianca Bueno <biancacunha326@gmail.com>
2025-08-10 14:37:38 -0300

    Criando a view, url e template para delete

--- project/urls.py
+++ project/urls.py
@@ -1,16 +1,28 @@
-# type:ignore
-from django.urls import path
+"""
+URL configuration for project project.
 
-from contact import views
-
-app_name = 'contact'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
-
-    # contact (CRUD)
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
-    path('contact/create/', views.create, name='create'),
-    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('', include('contact.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
